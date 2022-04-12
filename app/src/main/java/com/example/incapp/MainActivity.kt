168e3381bba04c5e270ec47e8b71af115f4729e5
Ankit Pal <ankitpal28090016@gmail.com>
2022-04-12 19:20:26 +0530

    final step

--- app/src/main/java/com/example/incapp/MainActivity.kt
+++ app/src/main/java/com/example/incapp/MainActivity.kt
@@ -7,27 +7,21 @@ import com.example.incapp.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
+    private var count=0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.plus.setOnClickListener { incrementOne() }
-        binding.reset.setOnClickListener { resetWzero() }
+        binding.plus.setOnClickListener {
+            count++
+            binding.inputNo.text=count.toString()
+
+                     }
+        binding.reset.setOnClickListener {
+            count =0
+            binding.inputNo.text=count.toString()
+        }
     }
 
-    private fun resetWzero() {
-        TODO("Not yet implemented")
-        val stringInTextField = binding.InputNo.text.toString()
-        val cost = stringInTextField.toDouble()
-        var incement = stringInTextField+1....
-    }
-
-    private fun incrementOne() {
-        TODO("Not yet implemented")
-        val stringInTextField = binding.InputNo.text.toString()
-        val cost = stringInTextField.toDouble()
-
-    }
 
 }
